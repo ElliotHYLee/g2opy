e99ea21089efac3f698bfb25ee3515f11cc410e0
el <hylee101001@gmail.com>
2018-11-17 21:39:25 -0500

    aasdf

---
+++ install.sh
@@ -0,0 +1,10 @@
+# for ubuntu 16 and python3
+#! /bin
+sudo apt-get install libglew1.6-dev libglewmx1.6-dev glew-utils libqt4-dev libsuitesparse-dev
+
+mkdir build
+cd build
+cmake ..
+make -j16
+cd ..
+sudo python3 setup.py install
