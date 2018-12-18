0833304a51dcf5b45a6df7e21d8a8492851a2759
el <hylee101001@gmail.com>
2018-12-18 18:28:50 -0500

    aasdf

--- install.sh
+++ install.sh
@@ -1,17 +1,18 @@
 # for ubuntu 16 and python3
 #! /bin
 
-# install http://eigen.tuxfamily.org/index.php?title=Main_Page 
+# install cmake
+# install eigen 3.3.4 only: http://eigen.tuxfamily.org/index.php?title=News:Eigen_3.3.4_released!
 
 
 
-sudo apt-get install cmake libglew1.6-dev libglewmx1.6-dev glew-utils libqt4-dev libsuitesparse-dev
+sudo apt-get install libglew1.6-dev libglewmx1.6-dev glew-utils libqt4-dev libsuitesparse-dev libsuitesparse-dev qtdeclarative5-dev qt5-qmake libqglviewer-dev
 
 sudo pip3 install PyOpenGL PyOpenGL_accelerate
 
 mkdir build
 cd build
 cmake ..
-make -j16
+make -j8
 cd ..
 sudo python3 setup.py install
