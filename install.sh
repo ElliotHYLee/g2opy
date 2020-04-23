1dfb2a387e2505478a88d653027c1523b01547e0
el <hylee101001@gmail.com>
2020-04-23 07:49:33 -0700

    update install

--- install.sh
+++ install.sh
@@ -5,9 +5,21 @@
 # install eigen 3.3.4 only: http://eigen.tuxfamily.org/index.php?title=News:Eigen_3.3.4_released!
 
 
-
+# system
+sudo apt install cmake
 sudo apt-get install libglew1.6-dev libglewmx1.6-dev glew-utils libqt4-dev libsuitesparse-dev libsuitesparse-dev qtdeclarative5-dev qt5-qmake libqglviewer-dev
 
+# unzip and install eigen
+tar -xvjf eigen-eigen-5a0156e40feb.tar.bz2
+cd eigen-eigen-5a0156e40feb
+mkdir build
+cd build
+cmake ..
+sudo make install 
+cd ../..
+
+# python3 part
+sudo apt install python3-pip
 sudo pip3 install PyOpenGL PyOpenGL_accelerate
 
 mkdir build
